805a60fe8e88a03c2fc4b6251605b3b92e311fde
“kaka5277” <254140626@qq.com>
2023-07-23 20:28:28 +1000

    ignore videos,results/assets,overwrite html/json

--- cypress/e2e/google.cy.js
+++ cypress/e2e/google.cy.js
@@ -1,7 +1,7 @@
 describe('google search', () => {
   it('google1', () => {
     cy.visit('https://google.com.au')
-   cy.get('[name="q"]').type('jenkins cypress{enter}')
+   cy.get('[name="q"]').type('jenkins cypress12{enter}')
    cy.url().should('contain','jenkins')
   })
 })

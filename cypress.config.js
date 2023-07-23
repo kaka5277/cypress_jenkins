a5d8342b61c8061322f08e998d34a222e3bcb39e
“kaka5277” <254140626@qq.com>
2023-07-23 19:40:29 +1000

    npm i mochawesome,results

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,13 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    
+      "reporter": "mochawesome",
+      "reporterOptions": {
+        "reportDir": "cypress/results",
+        "overwrite": false,
+        "html": false,
+        "json": true
+      }    
   },
 });
